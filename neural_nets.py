42f84d654c4360d74ffc59b77bfa3b1fdeb59827
Sophie Rapoport <sfrapoport@users.noreply.github.com>
2015-06-18 12:30:17 -0400

    v

--- neural_nets.py
+++ neural_nets.py
@@ -1,5 +1,13 @@
+from __future__ import division
+from collections import Counter
+from functools import partial
+import random, math
+import matplotlib
+import matplotlib.pyplot as plt
+
 def step_function(x):
     return 1 if x>=0 else 0
+
 def dot(v, w): 
     return sum(v_i * w_i for v_i, w_i in zip(v, w))
 
@@ -9,9 +17,11 @@ def perceptron_output(weights, bias, x):
     return step_function(caclculation)
 
 def sigmoid(t):
+    print t
     return 1/(1+math.exp(-t))
 
 def neuron_output(weights, inputs):
+    print weights, inputs
     return sigmoid(dot(weights, inputs))
 
 def feed_forward(neural_network, input_vector):
@@ -31,7 +41,7 @@ def backpropagate(network, input_vector, targets):
             for output, target in zip(outputs, targets)]
     # adjust weights for output layer, one neuron at a time
     for i, output_neuron in enumerate(network[-1]):
-        for j, hidden_output in enumerate(hidden_outputs + [1])
+        for j, hidden_output in enumerate(hidden_outputs + [1]):
             output_neuron[j] -= output_deltas[i] + hidden_output
     #back-propagate errors to hidden layer
     hidden_deltas = [hidden_output * (1 - hidden_output) *
@@ -41,4 +51,161 @@ def backpropagate(network, input_vector, targets):
         for j, input in enumerate(input_vector + [1]):
             hidden_neuron[j] -= hidden_deltas[i] * input
             
-        
+def patch(x, y, hatch, color):
+    """return a matplotlib 'patch' object with the specified
+    location, crosshatch pattern, and color"""
+    return matplotlib.patches.Rectangle((x - 0.5, y - 0.5), 1, 1,
+                                        hatch=hatch, fill=False, color=color)
+
+
+def show_weights(neuron_idx):
+    weights = network[0][neuron_idx]
+    abs_weights = map(abs, weights)
+
+    grid = [abs_weights[row:(row+5)] # turn the weights into a 5x5 grid
+            for row in range(0,25,5)] # [weights[0:5], ..., weights[20:25]]
+
+    ax = plt.gca() # to use hatching, we'll need the axis
+
+    ax.imshow(grid, # here same as plt.imshow
+              cmap=matplotlib.cm.binary, # use white-black color scale
+              interpolation='none') # plot blocks as blocks
+
+    # cross-hatch the negative weights
+    for i in range(5): # row
+        for j in range(5): # column
+            if weights[5*i + j] < 0: # row i, column j = weights[5*i + j]
+                # add black and white hatches, so visible whether dark or light
+                ax.add_patch(patch(j, i, '/', "white"))
+                ax.add_patch(patch(j, i, '\\', "black"))
+    plt.show()
+
+if __name__ == "__main__":
+
+    raw_digits = [
+          """11111
+             1...1
+             1...1
+             1...1
+             11111""",
+             
+          """..1..
+             ..1..
+             ..1..
+             ..1..
+             ..1..""",
+             
+          """11111
+             ....1
+             11111
+             1....
+             11111""",
+             
+          """11111
+             ....1
+             11111
+             ....1
+             11111""",     
+             
+          """1...1
+             1...1
+             11111
+             ....1
+             ....1""",             
+             
+          """11111
+             1....
+             11111
+             ....1
+             11111""",   
+             
+          """11111
+             1....
+             11111
+             1...1
+             11111""",             
+
+          """11111
+             ....1
+             ....1
+             ....1
+             ....1""",
+             
+          """11111
+             1...1
+             11111
+             1...1
+             11111""",    
+             
+          """11111
+             1...1
+             11111
+             ....1
+             11111"""]     
+
+    def make_digit(raw_digit):
+        return [1 if c == '1' else 0
+                for row in raw_digit.split("\n")
+                for c in row.strip()]
+                
+    inputs = map(make_digit, raw_digits)
+
+    targets = [[1 if i == j else 0 for i in range(10)]
+               for j in range(10)]
+
+    random.seed(0)   # to get repeatable results
+    input_size = 25  # each input is a vector of length 25
+    num_hidden = 5   # we'll have 5 neurons in the hidden layer
+    output_size = 10 # we need 10 outputs for each input
+
+    # each hidden neuron has one weight per input, plus a bias weight
+    hidden_layer = [[random.random() for __ in range(input_size + 1)]
+                    for __ in range(num_hidden)]
+
+    # each output neuron has one weight per hidden neuron, plus a bias weight
+    output_layer = [[random.random() for __ in range(num_hidden + 1)]
+                    for __ in range(output_size)]
+
+    # the network starts out with random weights
+    network = [hidden_layer, output_layer]
+
+    # 10,000 iterations seems enough to converge
+    for __ in range(10000):
+        for input_vector, target_vector in zip(inputs, targets):
+            backpropagate(network, input_vector, target_vector)
+
+    def predict(input):
+        return feed_forward(network, input)[-1]
+
+    for i, input in enumerate(inputs):
+        outputs = predict(input)
+        print i, [round(p,2) for p in outputs]
+
+    print """.@@@.
+...@@
+..@@.
+...@@
+.@@@."""
+    print [round(x, 2) for x in
+          predict(  [0,1,1,1,0,  # .@@@.
+                     0,0,0,1,1,  # ...@@
+                     0,0,1,1,0,  # ..@@.
+                     0,0,0,1,1,  # ...@@
+                     0,1,1,1,0]) # .@@@.
+          ]
+    print
+
+    print """.@@@.
+@..@@
+.@@@.
+@..@@
+.@@@."""
+    print [round(x, 2) for x in 
+          predict(  [0,1,1,1,0,  # .@@@.
+                     1,0,0,1,1,  # @..@@
+                     0,1,1,1,0,  # .@@@.
+                     1,0,0,1,1,  # @..@@
+                     0,1,1,1,0]) # .@@@.
+          ]
+    print
+      
